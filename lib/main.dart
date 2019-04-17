539e30b0a54bddaba811a1284bcd96fde20eb9cf
treetender <treetender@gmail.com>
2019-04-17 00:17:37 -0400

    Skipped issue where test does not work.  Updated app to fetch topstories via net

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'src/article.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert' as json;
 
 void main() => runApp(MyApp());
 
@@ -29,41 +30,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<int> _ids = [
-    19588996,
-    19583384,
-    19584921,
-    19584540,
-    19588852,
-    19584005,
-    19585033,
-    19588017,
-    19588744,
-    19584509,
-    19588395,
-    19585174,
-    19588961,
-    19582774,
-    19584440,
-    19561274,
-    19581721,
-    19577832,
-    19567962,
-    19585640,
-    19586219,
-    19588812,
-    19587782,
-    19583531
-  ];
+  List<int> _ids = [];
 
   Future<Article> _getArticle(int id) async {
-    final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/${id}.json';
-        final storyRes = await http.get(storyUrl);
+    final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
+    final storyRes = await http.get(storyUrl);
 
-        if (storyRes.statusCode == 200) {
-          return parseArticle(storyRes.body);
-        }
-      return null;
+    if (storyRes.statusCode == 200) {
+      return parseArticle(storyRes.body);
+    }
+    return null;
+  }
+
+  Future<void> _getTopStoryIds() async {
+    final idsUrl = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+    final idsRes = await http.get(idsUrl);
+
+    if (idsRes.statusCode == 200) {
+      var ids = List<int>.from(json.jsonDecode(idsRes.body));
+      setState(() {
+        _ids = ids;
+      });
+    }
   }
 
   @override
@@ -72,17 +60,23 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: ListView(
-        children: _ids.map((i) => FutureBuilder<Article>(
-          future: _getArticle(i),
-          builder: (BuildContext context, AsyncSnapshot<Article> snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return _buildItem(snapshot.data);
-            } else {
-              return Center(child: CircularProgressIndicator());
-            }
-          },)
-        ).toList(),
+      body: RefreshIndicator(
+        onRefresh: _getTopStoryIds,
+        child: ListView(
+          children: _ids
+              .map((i) => FutureBuilder<Article>(
+                    future: _getArticle(i),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<Article> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        return _buildItem(snapshot.data);
+                      } else {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                    },
+                  ))
+              .toList(),
+        ),
       ),
     );
   }
